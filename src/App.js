f9d924b8150f514bfa1c03140af2628eb32cdb42
maggielin18 <maggielin187@gmail.com>
2024-02-13 08:16:29 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "App">
+      <header className = "App-header">
+        <div className = "frame-container">
+          <div className = "frame frame1"></div>
+          <div className = "frame frame2"></div>
+          <div className = "frame frame3"></div>
+          <div className = "frame frame4"></div>
+          <div className = "frame frame5"></div>
+          <div className = "frame frame6"></div>
+          <div className = "frame frame7"></div>
+          <div className = "frame frame8"></div>
+          <div className = "frame frame9"></div>
+          <div className = "frame frame10"></div>
+          <div className = "frame frame11"></div>
+          <div className = "frame frame12"></div>
+          <div className = "frame frame13"></div>
+          <div className = "frame frame14"></div>
+          <div className = "frame frame15"></div>
+          <div className = "frame frame16"></div>
+          <div className = "frame frame17"></div>
+          <div className = "frame frame18"></div>
+
+          {/* <div className = "frame frame01"></div> */}
+          <div className = "frame frame02"></div>
+          <div className = "frame frame03"></div>
+          <div className = "frame frame04"></div>
+          <div className = "frame frame05"></div>
+          <div className = "frame frame06"></div>
+          <div className = "frame frame07"></div>
+          <div className = "frame frame08"></div>
+
+          <div className = "frame frame19"></div>
+          <div className = "frame frame20"></div>
+          <div className = "frame frame21"></div>
+          <div className = "frame frame22"></div>
+        </div>
       </header>
     </div>
   );
